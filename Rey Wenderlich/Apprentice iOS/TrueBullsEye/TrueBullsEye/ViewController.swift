93bc18ea1afb1d99a4a3a32ef3b8beb8351a19c9
b-towers-atl <b.towers.atl@gmail.com>
2015-02-24 19:48:30 -0500

    updates

--- Rey Wenderlich/Apprentice iOS/TrueBullsEye/TrueBullsEye/ViewController.swift
+++ Rey Wenderlich/Apprentice iOS/TrueBullsEye/TrueBullsEye/ViewController.swift
@@ -11,15 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     var currentValue = 0
     var targetValue = 0
+    var totalScore = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.startNewRound()
-        //        currentValue = lroundf(slider.value)
+        updateLabels()
         
     }
     
@@ -33,22 +38,33 @@ class ViewController: UIViewController {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        round += 1
         
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(totalScore)
+        roundLabel.text = "\(round)"
     }
     
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is \(targetValue)"
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        totalScore += points
         
+//        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is \(targetValue)"
+        
+        let message = "You scored \(points) points" + "\nSlider value = \(currentValue)" + "\nTarget value = \(targetValue)"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
-        
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
         alert.addAction(action)
-        
         presentViewController(alert, animated: true, completion: nil)
         
         self.startNewRound()
+        updateLabels()
         
     }
     
@@ -56,8 +72,39 @@ class ViewController: UIViewController {
         
         currentValue = lroundf(slider.value)
         
-        //        println("The value of the slider is now: \(slider.value)")
+//        println("The value of the slider is now: \(slider.value)")
         
     }
     
 }
+
+
+/*
+
+old code
+
+func compareValues() {
+    
+    var difference: Int
+    if currentValue > targetValue {
+        difference = currentValue - targetValue
+    } else if targetValue > currentValue {
+        difference = targetValue - currentValue
+    } else {
+        difference = 0
+    }
+    println(difference)
+    
+}
+
+
+func compareValues() {
+    
+    var difference = currentValue - targetValue
+    if difference < 0 {
+        difference = -difference
+    }
+    
+}
+
+*/
