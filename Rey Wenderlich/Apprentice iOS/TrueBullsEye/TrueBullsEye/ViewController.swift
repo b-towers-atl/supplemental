60d8bedb83430a56cbff50bd3bb3c885ccc819dc
b-towers-atl <b.towers.atl@gmail.com>
2015-02-25 22:04:14 -0500

    updates

--- Rey Wenderlich/Apprentice iOS/TrueBullsEye/TrueBullsEye/ViewController.swift
+++ Rey Wenderlich/Apprentice iOS/TrueBullsEye/TrueBullsEye/ViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // labels and sliders
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue = 0
-    var targetValue = 0
-    var totalScore = 0
-    var round = 0
+    // instance variables
+    var _currentValue = 0
+    var _targetValue = 0
+    var _totalScore = 0
+    var _round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.startNewRound()
-        updateLabels()
+        self.startNewGame()
+        
+        // custom slider
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackRightResizable, forState: .Normal)
+        }
         
     }
     
@@ -34,48 +54,90 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        // creates new random targetValue, resets slider, and increments round
         
-        targetValue = 1 + Int(arc4random_uniform(100))
-        currentValue = 50
-        slider.value = Float(currentValue)
-        round += 1
+        _targetValue = 1 + Int(arc4random_uniform(100))
+        _currentValue = 50
+        slider.value = Float(_currentValue)
+        _round += 1
+    }
+    
+    func startNewGame() {
+        // reset round, score and labels
         
+        _round = 0
+        _totalScore = 0
+        self.startNewRound()
+        self.updateLabels()
     }
     
     func updateLabels() {
-        targetLabel.text = String(targetValue)
-        scoreLabel.text = String(totalScore)
-        roundLabel.text = "\(round)"
+        // refreshes the labels with current values
+        
+        targetLabel.text = String(_targetValue)
+        scoreLabel.text = String(_totalScore)
+        roundLabel.text = "\(_round)"
     }
     
     @IBAction func showAlert() {
+        // Hit Me! button action
         
-        let difference = abs(currentValue - targetValue)
-        let points = 100 - difference
-        totalScore += points
+        // points variables
+        let difference = abs(_currentValue - _targetValue)
+        var points = 10 - difference
+        var bonusPoints = 0
         
-//        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is \(targetValue)"
+        // title conditionals for pop up alert
+        var title: String
+        if difference == 0 {
+            title = "Perfect Bull's Eye! BONUS!"
+            bonusPoints = 10
+        } else if difference < 3 {
+            title = "Almost perfect"
+            if difference == 1 {
+                bonusPoints = 6
+            }
+        } else if difference <= 5 {
+            title = "Pretty good"
+        } else if difference <= 10 {
+            title = "Decent try"
+        } else {
+            title = "Not even close..."
+        }
+        _totalScore += points + bonusPoints
         
-        let message = "You scored \(points) points" + "\nSlider value = \(currentValue)" + "\nTarget value = \(targetValue)"
-        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
-        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        // message variable, change message if bonus points are given
+        var message = "You scored \(points) points" + "\nSlider value = \(_currentValue)" + "\nTarget value = \(_targetValue)"
+        
+        if bonusPoints != 0 {
+            message = "You scored \(points) points and \(bonusPoints) bonus points!" + "\nTotal points = \(points + bonusPoints)" + "\nSlider value = \(_currentValue)" + "\nTarget value = \(_targetValue)"
+        }
+        
+        // alert created with added action, present alert view controller
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+            
+            // new round and refresh labels AFTER "OK" button is hit
+            self.startNewRound()
+            self.updateLabels()
+        })
         
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
-        
-        self.startNewRound()
-        updateLabels()
-        
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         
-        currentValue = lroundf(slider.value)
+        _currentValue = lroundf(slider.value)
         
-//        println("The value of the slider is now: \(slider.value)")
-        
+        // testing purposes
+        //println("The value of the slider is now: \(slider.value) \n\(currentValue)")
     }
     
+    @IBAction func restartButton(sender: UIButton) {
+        startNewGame()
+    }
+
 }
 
 
